e7c431cf9a842666c427bae97ebc57676ebe1ca2
Elden_Robbie <gillcharles97@gmail.com>
2025-11-13 22:04:24 -0500

    started working on a characters.py file which will have all playable characters and non_playable characters, along with their stats

---
+++ characters.py
@@ -0,0 +1,8 @@
+characters = {"name": {
+    "Jack": {"stats": {
+             "HP": 5}
+}}}
+#  "stats"HP": 5, "ATT": , "DEF": , "ACC": ,
+
+
+npcs = {}
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from extra import clear_screen
+from characters import characters
 
 def main():
-    m_c = introduction()
+    # I need to fix the characters file to be able to access "stats."
+    # I need to learn how to better use nested dictionaries and lists.
+    characters["name"][0] = introduction()
+    m_c = characters["name"][0]
     
 
 
 def introduction():
+    # This section is working as intended
+
     print("Welcome to the world!")
     nam = input("What's your name? ")
+    clear_screen()
     if nam.isdigit():
         print("That's not a name!")
     else:
