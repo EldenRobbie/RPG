b051440fedc77c96afbb724c463ed92381b96137
EldenRobbie <gillcharles97@gmail.com>
2025-12-07 02:12:28 -0500

    made the attack() and defend() functions for battle.py

--- battle.py
+++ battle.py
@@ -13,36 +13,78 @@ def battle():
     input("")
     clear_screen()
     print(f"{player.name} has {player.hp} HP.")
-    input("")
-    clear_screen()
-    print("What would you like to do?")
-    print("1. Attack, 2. Defend, 3. Item, 4. Flee ")
-    prompt = input("")
-    pmt = prompt.lower()
-    
-    if pmt == "1" or pmt == "attack":
-        print("You attack.")
-    
-    elif pmt == "2" or pmt == "defend":
-        print("You defend.")
+    while True:   
+        input("")
+        clear_screen()
+        print("What would you like to do?")
+        print("1. Attack, 2. Defend, 3. Item, 4. Flee ")
+        prompt = input("")
+        pmt = prompt.lower()
+        
+        if pmt == "1" or pmt == "attack":
+            attack()
+            if enemy_1.hp > 0:
+                continue
+            else:
+                break
+            
+        elif pmt == "2" or pmt == "defend":
+            defend()
+            if player.hp > 0:
+                continue
+            else:
+                break
 
-    elif pmt == "3" or pmt == "item":
-        print("You use an item.")
-    
-    elif pmt == "4" or pmt == "flee":
-        print("You flee.")
-    
-    else:
-        print("That's an invalid input.")
+
+        elif pmt == "3" or pmt == "item":
+            print("You use an item.")
+            return
+        
+        elif pmt == "4" or pmt == "flee":
+            clear_screen()
+            print("You flee.")
+            return
+        
+        else:
+            print("That's an invalid input.")
+            
 
 
 def attack():
-    pass
+    clear_screen()
+    print(f"{enemy_1.name}'s health is {enemy_1.hp}.")
+    print(f"{player.name} attacks the {enemy_1.name} for {player.attack} damage.")
+    enemy_1.hp -= player.attack
+    if enemy_1.hp <= 0:
+        clear_screen()
+        print("Victory!")
+        print("Doot doo doo doo, doo doo, doot doo doo!")
+        return 
+    else:
+        print(f"{enemy_1.name}'s hp is: {enemy_1.hp}.")
+
 
 def defend():
-    pass
+    clear_screen()
+    blocked_dmg = enemy_1.attack - player.defense
+    print(f"{player.name}'s hp is: {player.hp}, they defend!")
+    print(f"{enemy_1.name} attacks {player.name} for {blocked_dmg} damage.")
+    player.hp -= blocked_dmg
+    input("")
+    clear_screen()
+    if player.hp <= 0:
+        print("GAME OVER")
+        return 
+    else:
+        print(f"{player.name}'s hp is: {player.hp}.")
+
 
 def item():
     pass
+
+
+def flee():
+    pass
+
 
 main()
--- enemies.py
+++ enemies.py
@@ -1,12 +1,13 @@
 class Enemy:
-    def __init__(self, name, attack, defense, accuracy, xp_grant, item_grant):
+    def __init__(self, name, hp, attack, defense, accuracy, xp_grant, item_grant):
         self.name = name
+        self.hp = hp
         self.attack = attack
         self.defense = defense
         self.accuracy = accuracy
         self.xp_grant = xp_grant
         self.item_grant = item_grant
 
-enemy_1 = Enemy("slime", 1, 0, 1, 2, [])
+enemy_1 = Enemy("slime", 3, 1, 0, 1, 2, [])
 
 
